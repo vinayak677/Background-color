5dde2740b16bd107f659dd9435257c78d863fdd6
vinayak677 <95090050+vinayak677@users.noreply.github.com>
2022-03-01 12:02:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var css=document.querySelector("h3");
+var color1=document.querySelector(".color1");
+var color2=document.querySelector(".color2");
+var body=document.getElementById("main");
+
+function color(){
+    body.style.background="linear-gradient(to right,"
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+}
+
+color1.addEventListener("input", color);
+
+color2.addEventListener("input", color);
+    
